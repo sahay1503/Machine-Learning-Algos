c07ebd23206572ef0e047119e78b69e77548e47c
Siddhant Sahay <113369476+sahay1503@users.noreply.github.com>
2023-12-13 08:03:05 +0530

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -33,3 +33,18 @@ ax.set_xlabel('Sepal length(cm)')
 ax.set_ylabel('Sepal width (cm)')
 ax.grid()
 ax.legend()
+
+from sklearn.model_selection import train_test_split
+X = iris.drop(['sepal length (cm)','sepal width (cm)','target','species'],axis=1)
+y =iris.target
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=82)
+
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+kernels=['linear','rbf','poly']
+for kernel in kernels:
+  model = SVC(kernel=kernel)
+  model.fit(X_train,y_train)
+  pred=model.predict(X_test)
+  print("acuracy using {}:".format(kernel),accuracy_score(pred,y_test))
